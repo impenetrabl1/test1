12b37c9132aa8da9d2856104654b3fea6b853fda
impenetrabl1 <qwerqweqwr214@gmail.com>
2019-04-18 13:34:23 +0300

    task1 change commit

--- task1.py
+++ task1.py
@@ -5,5 +5,8 @@ list1 = [random.randrange(10) for i in range(0, 10)]
 
 def func1(list1):
     list2 = [list1.count(j) for j in list1]
-    print(list2)
     return list1[list2.index(max(list2))]
+
+
+print(list1)
+print(func1(list1))
