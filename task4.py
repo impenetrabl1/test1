eb82f72f71655164adfd591b7f53742e1fab3c40
Alexandr <qwerqweqwr214@gmail.com>
2019-04-20 00:52:57 +0300

    fixed task4 with using my ContextManager

--- task4.py
+++ task4.py
@@ -1,23 +1,26 @@
 import calendar
+from ContextManager import FileOpen
+
+with FileOpen(r'.\dataFiles\task4_data.txt', 'r') as open_file:
+    given_year = int(open_file.read())
 
 
-def func4(year):
+def given_year_is_leap(year):
     if year % 4 != 0:
-        print('обычный')
+        return False
     elif year % 100 != 0:
-        print('высокосный')
+        return True
     elif year % 400 != 0:
-        print('обычный')
-    else:
-        print('высокосный')
+        return False
+    return True
 
 
-def func4_2(year):
+def given_year_is_leap_2(year):
     if calendar.isleap(year):
-        print('высокосный')
-    else:
-        print('обычный')
+        return True
+    return False
 
 
-func4(2019)
-func4_2(2019)
+with FileOpen(r'.\dataFiles\task4_data.txt', 'a') as open_file:
+    open_file.write('\nIs given year is leap? ' + str(given_year_is_leap(given_year)))
+    open_file.write('\nIs given year is leap? ' + str(given_year_is_leap_2(given_year)))
