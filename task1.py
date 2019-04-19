0aa566dd483a8b994603b22b4f4ad98fb4427167
impenetrabl1 <qwerqweqwr214@gmail.com>
2019-04-19 17:43:37 +0300

    second fix task1 with using my ContextManager

--- task1.py
+++ task1.py
@@ -1,6 +1,6 @@
 from ContextManager import FileOpen
 
-with FileOpen(r'C:\Users\Aliaksandr.Baryhin\Desktop\dataFiles\task1_data.txt') as open_file:
+with FileOpen(r'C:\Users\Aliaksandr.Baryhin\Desktop\dataFiles\task1_data.txt', 'r') as open_file:
     list_of_numbers_from_file = list(map(int, open_file.read().split(' ')))
 
 
@@ -9,5 +9,5 @@ def max_count_element(processing_list):
     return processing_list[count_list.index(max(count_list))]
 
 
-print(list_of_numbers_from_file)
-print(max_count_element(list_of_numbers_from_file))
+with FileOpen(r'C:\Users\Aliaksandr.Baryhin\Desktop\dataFiles\task1_data.txt', 'a') as open_file:
+    open_file.write('\nMax element is: ' + str(max_count_element(list_of_numbers_from_file)))
