b20cad68a082e3c7765c1b150018d33e1fae8a6b
impenetrabl1 <qwerqweqwr214@gmail.com>
2019-04-22 17:03:00 +0300

    fixed task6 with using my ContextManager

--- task6.py
+++ task6.py
@@ -1,14 +1,25 @@
 import time
+from ContextManager import FileOpen
 
-n = 10000
-
-
-def func6(n):
-    num = [i ** i for i in range(1, n + 1)]
-    str1 = str(sum(num))
-    return str1[-10:]
+with FileOpen(r'C:\Users\Aliaksandr.Baryhin\Desktop\dataFiles\task6_data.txt', 'r') as open_file:
+    number = int(open_file.read())
 
 
-start = time.time()
-print(func6(n))
-print((time.time() - start))
+def function_time(func):
+    def wrapper(arg):
+        start = time.time()
+        value = func(arg)
+        print((time.time() - start))
+        return value
+    return wrapper
+
+
+@function_time
+def calc_range(n):
+    squares = [i ** i for i in range(1, n + 1)]
+    sum1 = str(sum(squares))
+    return sum1[-10:]
+
+
+with FileOpen(r'C:\Users\Aliaksandr.Baryhin\Desktop\dataFiles\task6_data.txt', 'a') as open_file:
+    open_file.write('\nLast 10 numbers of range: '+str(calc_range(number)))
